f1b12fb9b4273cf38f22b378aff55b00cb1bea9f
Shaitanova <0984599085@mail.ru>
2018-07-19 18:40:21 +0300

    Update SecurityInitializer.java

--- src/main/java/com/project/eva/config/SecurityInitializer.java
+++ src/main/java/com/project/eva/config/SecurityInitializer.java
@@ -2,18 +2,6 @@ package com.project.eva.config;
 
 import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
 
-/**
- * Класс наследующий AbstractSecurityWebApplicationInitializer.
- * Класс нужен для того, чтобы удостовериться, что настройки безопасности включены в основной
- * контекст приложения (их увидел и втянул в себя Root Application Context).
- * Для этого нужен этот класс. Нам нужно настроить все так чтобы определенный
- * URL проходил через уровень безопасности.
- *
 
- * @version 1.2
- * @see SecurityConfig
- * @see RootConfig
- * @see WebConfig
- */
 public class SecurityInitializer extends AbstractSecurityWebApplicationInitializer {
 }
